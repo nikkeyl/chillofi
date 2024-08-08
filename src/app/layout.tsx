1dbdd0300373be639f8d2ab57efc55f59f9a754f
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-08 19:23:31 +0300

    refactor: small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './style.scss';
+import '@app/style.scss';
 
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
@@ -6,7 +6,14 @@ import type { PropsWithChildren } from 'react';
 const metadata: Metadata = {
   title: 'Chillofi',
   description: 'LOFI-radio for work, study and relax',
-  icons: { icon: 'public/favicon.ico' },
+  icons: {
+    icon: {
+      url: 'public/favicon.ico',
+      type: 'image/x-icon',
+      sizes: '48x48',
+      fetchPriority: 'high',
+    },
+  },
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.scss';
+import styles from '@/app/page.module.scss';
 
 const Home = () => {};
 
