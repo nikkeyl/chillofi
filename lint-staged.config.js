d09676db57dfefdc9ea3b582a661588738997d34
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-18 16:24:30 +0300

    refactor: small changes

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,5 @@
 export default {
-  '**/*.md': 'remark --quiet --frail',
-  '**/*': 'prettier --write',
+  '*': 'prettier --write',
   'src/**/*.{tsx,ts}': 'eslint --fix',
   'src/**/*.scss': 'stylelint --fix',
 };
