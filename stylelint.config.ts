8672efc1d46b95afb23c6f02cdc83d98d9c848c1
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-08-20 16:39:57 +0300

    refactor(stylelint): add options

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -4,6 +4,13 @@ export default defineConfig({
   extends: '@archoleat/stylelint-config-extended-scss',
   rules: {
     'declaration-empty-line-before': null,
+    'no-global-function-names': null,
     'selector-class-pattern': null,
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
   },
 });
