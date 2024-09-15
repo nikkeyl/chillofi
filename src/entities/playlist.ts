54b707cb5f006f83009d4886981230015ee5d224
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-09-15 17:28:16 +0300

    feat: robots and loop for playlist

--- src/entities/playlist.ts
+++ src/entities/playlist.ts
@@ -1,103 +1,35 @@
 const audioFolder = `audio`;
-const musicFolder = `${audioFolder}/music`;
 
-const playList = [
-  {
-    src: `${musicFolder}/bedhead.mp3`,
-    id: 1,
-  },
-  {
-    src: `${musicFolder}/cats-cradle.mp3`,
-    id: 2,
-  },
-  {
-    src: `${musicFolder}/crescent-moon.mp3`,
-    id: 3,
-  },
-  {
-    src: `${musicFolder}/dreams-come-true.mp3`,
-    id: 4,
-  },
-  {
-    src: `${musicFolder}/field-of-fireflies.mp3`,
-    id: 5,
-  },
-  {
-    src: `${musicFolder}/floating-castle.mp3`,
-    id: 6,
-  },
-  {
-    src: `${musicFolder}/forgotten-places.mp3`,
-    id: 7,
-  },
-  {
-    src: `${musicFolder}/green-tea.mp3`,
-    id: 8,
-  },
-  {
-    src: `${musicFolder}/heart-of-the-ocean.mp3`,
-    id: 9,
-  },
-  {
-    src: `${musicFolder}/in-the-past.mp3`,
-    id: 10,
-  },
-  {
-    src: `${musicFolder}/journey-end.mp3`,
-    id: 11,
-  },
-  {
-    src: `${musicFolder}/lazy-afternoon.mp3`,
-    id: 12,
-  },
-  {
-    src: `${musicFolder}/lost-and-found.mp3`,
-    id: 13,
-  },
-  {
-    src: `${musicFolder}/midnight-snack.mp3`,
-    id: 14,
-  },
-  {
-    src: `${musicFolder}/missing-you.mp3`,
-    id: 15,
-  },
-  {
-    src: `${musicFolder}/moonlight-walk.mp3`,
-    id: 16,
-  },
-  {
-    src: `${musicFolder}/serendipity.mp3`,
-    id: 17,
-  },
-  {
-    src: `${musicFolder}/signs-of-life.mp3`,
-    id: 18,
-  },
-  {
-    src: `${musicFolder}/silent-wood.mp3`,
-    id: 19,
-  },
-  {
-    src: `${musicFolder}/sleepless.mp3`,
-    id: 20,
-  },
-  {
-    src: `${musicFolder}/storm-clouds.mp3`,
-    id: 21,
-  },
-  {
-    src: `${musicFolder}/wanted.mp3`,
-    id: 22,
-  },
-  {
-    src: `${musicFolder}/where-the-waves-take-us.mp3`,
-    id: 23,
-  },
-  {
-    src: `${musicFolder}/wild-strawberry.mp3`,
-    id: 24,
-  },
+const audios = [
+  'bedhead',
+  'cats-cradle',
+  'crescent-moon',
+  'dreams-come-true',
+  'field-of-fireflies',
+  'floating-castle',
+  'forgotten-places',
+  'green-tea',
+  'heart-of-the-ocean',
+  'in-the-past',
+  'journey-end',
+  'lazy-afternoon',
+  'lost-and-found',
+  'midnight-snack',
+  'missing-you',
+  'moonlight-walk',
+  'serendipity',
+  'signs-of-life',
+  'silent-wood',
+  'sleepless',
+  'storm-clouds',
+  'wanted',
+  'where-the-waves-take-us',
+  'wild-strawberry',
 ];
+
+const playList = audios.map((file, index) => ({
+  src: `${audioFolder}/music/${file}.mp3`,
+  id: +index,
+}));
 
 export { playList };
