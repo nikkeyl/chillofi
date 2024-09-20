e4c1ee61a6e0b71e6172fca8e9cf50d7f2c2dc0f
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-09-20 13:43:33 +0300

    refactor: player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,16 @@ import { Player } from '@ui';
 
 import background from '@images/background.jpg';
 
+// Image for mobile
+// Click on touchpad
+// Meta
+// Favicon
+// Tests
+// Lighthouse performance
+// Stylelint
+// Funding
+// seo
+// i18n for alt
 const HomePage = () => (
   <Screen>
     <Image src={background} alt='background' priority fetchPriority='high' />
