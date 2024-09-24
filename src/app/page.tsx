eded2b823c35f7d4edccf39fd2bdd1079fb64584
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-09-24 20:38:02 +0300

    refactor: remove comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,6 @@ import { Player } from '@ui';
 
 import background from '@images/background.jpg';
 
-// Image for mobile
-// Click on touchpad
-// Meta
-// Favicon
-// Tests
-// Lighthouse performance
-// Stylelint
-// Funding
-// Cypress
-// Security
-// seo
-// i18n for alt
 const HomePage = () => (
   <Screen>
     <Image src={background} alt='background' priority fetchPriority='high' />
