076ac8fac473d8e71394f7486e8709b8ece6c7b7
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-11-04 20:22:33 +0300

    refactor: small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 import { ScreenProvider } from '@providers';
-import { Panel, Wrapper } from '@ui';
+import { Panel } from '@ui';
 import { Logo, Player, Screen, Switcher } from '@widgets';
 
 const HomePage = () => (
   <ScreenProvider>
-    <Wrapper>
-      <Screen />
-      <Panel>
-        <Switcher type='crt' />
-        <Player />
-        <Switcher type='image' />
-        <Logo />
-      </Panel>
-    </Wrapper>
+    <Screen />
+    <Panel>
+      <Switcher type='crt' />
+      <Player />
+      <Switcher type='image' />
+      <Logo />
+    </Panel>
   </ScreenProvider>
 );
 
