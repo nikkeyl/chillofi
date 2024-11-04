8dd3dd1f15595872543a1fd3061c031d4640b861
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-11-04 14:39:42 +0300

    refactor: small changes

--- src/widgets/switcher/ui/switcher.tsx
+++ src/widgets/switcher/ui/switcher.tsx
@@ -3,8 +3,6 @@
 import { accessibilityLabels, sounds } from '@data';
 import { useScreenContext } from '@providers';
 import { Button } from '@ui';
-import classes from 'classnames';
-import { useEffect, useState } from 'react';
 import useSound from 'use-sound';
 
 import style from './switcher.module.scss';
@@ -17,32 +15,23 @@ const Switcher = (properties: SwitcherProperties) => {
   const { switcherSound } = sounds;
   const { setCRTEffect, setNextImage } = useScreenContext();
 
-  const [isActive, setIsActive] = useState(false);
   const [playSound] = useSound(switcherSound);
 
   const handleClick = () => {
-    setIsActive(!isActive);
     playSound();
-  };
 
-  useEffect(() => {
-    if (isActive) {
-      const timer = setTimeout(() => {
-        setIsActive(false);
-      }, 200);
-
-      return () => clearTimeout(timer);
+    if (type === 'image') {
+      setNextImage();
+    } else {
+      setCRTEffect();
     }
-  }, [isActive]);
+  };
 
   return (
     <Button
       ariaLabel={switcherControlLabel}
-      className={classes(style.switcher, isActive && style.active)}
-      onClick={() => {
-        handleClick();
-        type === 'image' ? setNextImage() : setCRTEffect();
-      }}
+      className={style.switcher ?? ''}
+      onClick={handleClick}
     />
   );
 };
