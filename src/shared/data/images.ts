52ca61bdb1b4c6780f4f088c081a2ae2abccc80f
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-11-05 22:17:40 +0300

    feat: add new image

--- src/shared/data/images.ts
+++ src/shared/data/images.ts
@@ -2,7 +2,8 @@ import screenOne from '@images/screen-1.jpg';
 import screenTwo from '@images/screen-2.jpg';
 import screenThree from '@images/screen-3.jpg';
 import screenFour from '@images/screen-4.jpg';
+import screenFive from '@images/screen-5.jpg';
 
-const images = [screenOne, screenTwo, screenThree, screenFour];
+const images = [screenOne, screenTwo, screenThree, screenFour, screenFive];
 
 export { images };
