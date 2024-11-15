5df0f52eec1effbb213ca32d86462b83550eddc4
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-15 15:25:13 +0300

    refactor: optimize images

--- src/shared/data/images.ts
+++ src/shared/data/images.ts
@@ -3,7 +3,19 @@ import screenTwo from '@images/screen-2.webp';
 import screenThree from '@images/screen-3.webp';
 import screenFour from '@images/screen-4.webp';
 import screenFive from '@images/screen-5.webp';
+import screenSix from '@images/screen-6.webp';
+import screenSeven from '@images/screen-7.webp';
+import screenEight from '@images/screen-8.webp';
 
-const images = [screenOne, screenTwo, screenThree, screenFour, screenFive];
+const images = [
+  screenOne,
+  screenTwo,
+  screenThree,
+  screenFour,
+  screenFive,
+  screenSix,
+  screenSeven,
+  screenEight,
+];
 
 export { images };
