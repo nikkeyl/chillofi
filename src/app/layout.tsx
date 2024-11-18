6ccbd67ee848dbc5f24f2b7a18b5bf248544be69
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 16:30:15 +0300

    refactor: small changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,23 +6,22 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import type { PropsWithChildren } from 'react';
 
-import { ScreenCRTEffectProvider /* , ScreenImagesProvider */ } from '@/providers';
-// import { getImages } from '@/server';
+import { ScreenCRTEffectProvider, ScreenImagesProvider } from '@/providers';
+import { getImages } from '@/server';
 
 const RootLayout = async (properties: PropsWithChildren) => {
   const { children } = properties;
 
   const locale = await getLocale();
   const messages = await getMessages();
-  // const images = await getImages();
+  const images = await getImages();
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
           <ScreenCRTEffectProvider>
-            {children}
-            {/* <ScreenImagesProvider images={images}>{children}</ScreenImagesProvider> */}
+            <ScreenImagesProvider images={images}>{children}</ScreenImagesProvider>
           </ScreenCRTEffectProvider>
         </NextIntlClientProvider>
         <Analytics />
