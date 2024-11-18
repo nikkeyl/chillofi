ca7abdd3c0e5e5d0b2f76ec4a5c71354ba289c2b
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 17:09:41 +0300

    refactor: small changes

--- src/app/server/get-locales.ts
+++ src/app/server/get-locales.ts
@@ -1,5 +1,5 @@
-import { readFile } from 'node:fs/promises';
-import { join } from 'node:path';
+// import { readFile } from 'node:fs/promises';
+// import { join } from 'node:path';
 
 import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
@@ -10,17 +10,17 @@ export default getRequestConfig(async () => {
   const cleanLocales = browserLocale.split(',').map((locale) => locale.trim());
   const defineLocale = cleanLocales[0]?.split('-')[0] ?? 'en';
   const locale = supportedLocales.includes(defineLocale) ? defineLocale : 'en';
-  const localesFile = join(
-    process.cwd(),
-    `public/locales/${locale}/translations.json`,
-  );
+  // const localesFile = join(
+  //   process.cwd(),
+  //   `public/locales/${locale}/translations.json`,
+  // );
 
-  const translations = await readFile(localesFile, 'utf-8');
-  const messages = JSON.parse(translations);
+  // const translations = await readFile(localesFile, 'utf-8');
+  // const messages = JSON.parse(translations);
 
   return {
     locale,
-    messages,
+    messages: await import(`public/locales/${locale}/translations.json`),
   };
   // try {
   // } catch (error: any) {
