2f4371af7edf3af3783de98ddb6e71b5f9dd5e58
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 15:23:29 +0300

    fix: remove extra await

--- src/app/server/get-locales.ts
+++ src/app/server/get-locales.ts
@@ -2,7 +2,7 @@ import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
-  const browserLocale = (await headers().get('accept-language')) ?? 'en';
+  const browserLocale = headers().get('accept-language') ?? 'en';
   const supportedLocales = ['de', 'en', 'ja', 'ru'];
   const locales = browserLocale.split(',').map((locale) => locale.trim());
   const locale = locales[0]?.split('-')[0] ?? 'en';
