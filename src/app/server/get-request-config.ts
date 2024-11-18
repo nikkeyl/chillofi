c3dccf4f62d2775f1428e50e89dc7dd2d006364e
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-19 02:07:41 +0300

    refactor: small changes

--- src/app/server/get-request-config.ts
+++ src/app/server/get-request-config.ts
@@ -5,31 +5,29 @@ import { join } from 'node:path';
 import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
-const getSupportedLocales = async () => {
-  const localesDirectory = join(process.cwd(), '/public/locales');
-  const directories = await readdir(localesDirectory, { withFileTypes: true });
-
-  return directories
-    .filter((directory) => directory.isDirectory())
-    .map((directory) => directory.name);
-};
-
-// import { getSupportedLocales } from './get-supported-locales';
-
-const getLocale = async () => {
-  const browserLocale = headers().get('accept-language') ?? 'en';
-  const supportedLocales = await getSupportedLocales();
-
-  const defineLocale =
-    browserLocale
-      .split(',')
-      .map((locale) => locale.trim())[0]
-      ?.split('-')[0] ?? 'en';
-
-  return supportedLocales.includes(defineLocale) ? defineLocale : 'en';
-};
-
 export default getRequestConfig(async () => {
+  const getSupportedLocales = async () => {
+    const localesDirectory = join(process.cwd(), 'public/locales');
+    const directories = await readdir(localesDirectory, { withFileTypes: true });
+
+    return directories
+      .filter((directory) => directory.isDirectory())
+      .map((directory) => directory.name);
+  };
+
+  const getLocale = async () => {
+    const browserLocale = headers().get('accept-language') ?? 'en';
+    const supportedLocales = await getSupportedLocales();
+
+    const defineLocale =
+      browserLocale
+        .split(',')
+        .map((locale) => locale.trim())[0]
+        ?.split('-')[0] ?? 'en';
+
+    return supportedLocales.includes(defineLocale) ? defineLocale : 'en';
+  };
+
   const locale = await getLocale();
 
   return {
