f2c4a2b4230a910717a64799292f31e3efcfa941
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 17:16:39 +0300

    refactor: small changes

--- src/app/server/get-locales.ts
+++ src/app/server/get-locales.ts
@@ -1,6 +1,3 @@
-// import { readFile } from 'node:fs/promises';
-// import { join } from 'node:path';
-
 import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
@@ -10,24 +7,9 @@ export default getRequestConfig(async () => {
   const cleanLocales = browserLocale.split(',').map((locale) => locale.trim());
   const defineLocale = cleanLocales[0]?.split('-')[0] ?? 'en';
   const locale = supportedLocales.includes(defineLocale) ? defineLocale : 'en';
-  console.log(locale);
-  // const localesFile = join(
-  //   process.cwd(),
-  //   `public/locales/${locale}/translations.json`,
-  // );
-
-  // const translations = await readFile(localesFile, 'utf-8');
-  // const messages = JSON.parse(translations);
 
   return {
     locale,
     messages: (await import(`/public/locales/${locale}/translations.json`)).default,
   };
-  // try {
-  // } catch (error: any) {
-  //   return {
-  //     message: `Failed to load translations, locale: ${locale} [${error.message}]`,
-  //     statusCode: 500,
-  //   };
-  // }
 });
