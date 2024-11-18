c8dc4e3dc5ebfb7b85b528837785cb71747784c0
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 17:35:50 +0300

    refactor: small changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
-import { ScreenCRTEffectProvider, ScreenImagesProvider } from '@/providers';
-import { getImages } from '@/server';
 import { Panel } from '@/ui';
 import { Player, Screen, Switcher } from '@/widgets';
 
-const HomePage = async () => {
-  const images = await getImages();
-
-  return (
-    <ScreenCRTEffectProvider>
-      <ScreenImagesProvider images={images}>
-        <Screen />
-        <Panel>
-          <Switcher type='crt' />
-          <Player />
-          <Switcher type='image' />
-        </Panel>
-      </ScreenImagesProvider>
-    </ScreenCRTEffectProvider>
-  );
-};
+const HomePage = () => (
+  <>
+    <Screen />
+    <Panel>
+      <Switcher type='crt' />
+      <Player />
+      <Switcher type='image' />
+    </Panel>
+  </>
+);
 
 export default HomePage;
