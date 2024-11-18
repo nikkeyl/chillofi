a674e0de2247c79b7fd4fe15ce970b9e25d306ed
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-18 17:31:40 +0300

    fix: production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,23 +6,17 @@ import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
 import type { PropsWithChildren } from 'react';
 
-import { ScreenCRTEffectProvider, ScreenImagesProvider } from '@/providers';
-import { getImages } from '@/server';
-
 const RootLayout = async (properties: PropsWithChildren) => {
   const { children } = properties;
 
   const locale = await getLocale();
   const messages = await getMessages();
-  const images = await getImages();
 
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
-          <ScreenCRTEffectProvider>
-            <ScreenImagesProvider images={images}>{children}</ScreenImagesProvider>
-          </ScreenCRTEffectProvider>
+          {children}
         </NextIntlClientProvider>
         <Analytics />
         <SpeedInsights />
