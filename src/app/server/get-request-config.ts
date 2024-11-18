24ce512c6e0c9636890cb5a11f235e4556f74117
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-19 02:04:34 +0300

    refactor: small changes

--- src/app/server/get-request-config.ts
+++ src/app/server/get-request-config.ts
@@ -1,6 +1,33 @@
+import { readdir } from 'node:fs/promises';
+import { join } from 'node:path';
+
+// import { getLocale } from './get-locale';
+import { headers } from 'next/headers';
 import { getRequestConfig } from 'next-intl/server';
 
-import { getLocale } from './get-locale';
+const getSupportedLocales = async () => {
+  const localesDirectory = join(process.cwd(), 'public/locales');
+  const directories = await readdir(localesDirectory, { withFileTypes: true });
+
+  return directories
+    .filter((directory) => directory.isDirectory())
+    .map((directory) => directory.name);
+};
+
+// import { getSupportedLocales } from './get-supported-locales';
+
+const getLocale = async () => {
+  const browserLocale = headers().get('accept-language') ?? 'en';
+  const supportedLocales = await getSupportedLocales();
+
+  const defineLocale =
+    browserLocale
+      .split(',')
+      .map((locale) => locale.trim())[0]
+      ?.split('-')[0] ?? 'en';
+
+  return supportedLocales.includes(defineLocale) ? defineLocale : 'en';
+};
 
 export default getRequestConfig(async () => {
   const locale = await getLocale();
