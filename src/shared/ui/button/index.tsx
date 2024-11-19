58a9aa07885db18baaa82bf828769d5b5e03d273
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-11-19 22:29:52 +0300

    refactor: small changes

--- src/shared/ui/button/index.tsx
+++ src/shared/ui/button/index.tsx
@@ -1,10 +1,19 @@
+'use client';
+
 import classes from 'classnames';
+import { useEffect, useState } from 'react';
 
 import style from './button.module.scss';
 import type { ButtonProperties } from './button.properties';
 
 const Button = (properties: ButtonProperties) => {
   const { ariaLabel, isActive, type = 'switch', onClick } = properties;
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    setIsDisabled(false);
+  }, []);
 
   return (
     <button
@@ -14,6 +23,7 @@ const Button = (properties: ButtonProperties) => {
         type === 'switch' ? style.circle : style.rectangle,
         isActive && style.active,
       )}
+      disabled={isDisabled}
       onClick={onClick}
       type='button'
     />
